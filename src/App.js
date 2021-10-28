a6992bc4648fdc484c41f729b57b12611ebde78d
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-28 01:03:08 -0400

    Click tap to change content unsolved Css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
+const arr = [
+  { label: "Tap 1", content: "This is content 1", isActive: false },
+  { label: "Tap 2", content: "This is content 2", isActive: false },
+  { label: "Tap 3", content: "This is content 3", isActive: false },
+];
 function App() {
+  const [content, setContent] = useState("");
+  const [contentArr, setContentArr] = useState([...arr]);
+  const onClickTap = (e, item) => {
+    item.isActive = true;
+    setContent(item.content);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {contentArr.map((item, i) => {
+        return (
+          <button
+            className="btn"
+            onClick={(e) => onClickTap(e, item)}
+          >
+            {item.label}
+          </button>
+        );
+      })}
+      <div className="content-container">
+        <h2>{content}</h2>
+      </div>
     </div>
   );
 }
